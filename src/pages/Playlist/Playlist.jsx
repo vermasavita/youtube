25da6b8d09fd81aff0a833bebf2d57b9373a0f2a
Savita Verma <svitaverma10@gmail.com>
2022-05-05 14:47:07 +0530

    feat: add routehandler

--- src/pages/Playlist/Playlist.jsx
+++ src/pages/Playlist/Playlist.jsx
@@ -1,20 +1,26 @@
 import { PlaylistCard } from "./component/PlaylistComp";
 import { DisplayPlaylist } from "./component/DisplayPlaylist";
+import { SideBar } from "../../components";
 
 const Playlist = () => {
-    return(
-        <>
+  return (
+    <div className="video-listing-container">
+      <div>
+        <SideBar />
+      </div>
+      <div>
         <h1>Playlist</h1>
         <div className="playlist-container">
-            <DisplayPlaylist/>
-            <DisplayPlaylist/>
-            <DisplayPlaylist/>
-            <DisplayPlaylist/>
-            <DisplayPlaylist/>
-            <DisplayPlaylist/>
+          <DisplayPlaylist />
+          <DisplayPlaylist />
+          <DisplayPlaylist />
+          <DisplayPlaylist />
+          <DisplayPlaylist />
+          <DisplayPlaylist />
         </div>
-        </>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export { Playlist};
+export { Playlist };
