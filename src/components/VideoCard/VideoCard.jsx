3e0de24d7a0ffa8d2ed299c69c0e6861d5d41efe
Savita Verma <svitaverma10@gmail.com>
2022-05-07 21:03:39 +0530

    feat: add action component

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,14 +1,20 @@
 import "./videoCard.css";
+import { ActionCard } from "./ActionCard";
+import { useState } from "react";
 
-const VideoCard = ({ videoId, videoTitle, videothumbnail, videoLength, videoCreator }) => {
-
+const VideoCard = ({
+  videoId,
+  videoTitle,
+  videothumbnail,
+  videoLength,
+  videoCreator,
+}) => {
+  const [actionBox, setActionBox] = useState(false);
+ 
   return (
-    <div className="video-card-container" key={videoId} >
+    <div className="video-card-container" key={videoId}>
       <div className="video-image">
-        <img
-          src={videothumbnail}
-          alt={videoTitle}
-        />
+        <img src={videothumbnail} alt={videoTitle} />
         <div className="video-time">{videoLength}</div>
       </div>
       <div className="video-info">
@@ -20,9 +26,9 @@ const VideoCard = ({ videoId, videoTitle, videothumbnail, videoLength, videoCrea
           />
         </div>
         <div className="video-content">
-        {videoTitle} |<span> {videoCreator}</span>
+          {videoTitle} |<span> {videoCreator}</span>
         </div>
-        <div className="action-icons">
+        <div className="action-icons" onClick={() => setActionBox((prev) => !prev)}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="icon"
@@ -31,9 +37,9 @@ const VideoCard = ({ videoId, videoTitle, videothumbnail, videoLength, videoCrea
             <path d="M64 360C94.93 360 120 385.1 120 416C120 446.9 94.93 472 64 472C33.07 472 8 446.9 8 416C8 385.1 33.07 360 64 360zM64 200C94.93 200 120 225.1 120 256C120 286.9 94.93 312 64 312C33.07 312 8 286.9 8 256C8 225.1 33.07 200 64 200zM64 152C33.07 152 8 126.9 8 96C8 65.07 33.07 40 64 40C94.93 40 120 65.07 120 96C120 126.9 94.93 152 64 152z" />
           </svg>
         </div>
+        {actionBox && <ActionCard />}
       </div>
-      </div>
-
+    </div>
   );
 };
 
